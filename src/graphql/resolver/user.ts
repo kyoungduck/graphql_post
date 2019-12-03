8645a3309451d2448aa99a5ada3ea523f5b71b46
KyoungDuck <skate2@naver.com>
2019-12-03 19:02:10 +0900

    eslint 업데이트 및 graphql쿼리 처리

--- src/graphql/resolver/user.ts
+++ src/graphql/resolver/user.ts
@@ -3,5 +3,16 @@ import _ from 'lodash';
 import { ApolloError } from 'apollo-server';
 
 export default {
+    Query : {
+        auth : (parant: any, args: any, context: any) => {
+            console.log('auth called');
+            context.auth = 'logined';
+            return true;
+        },
 
+        afterAuth : (parant: any, args: any, context: any) => {
+            console.log(context);
+            return 'test';
+        }
+    }
 };
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import typeDefs from '@gql/schema/index';
 // Initialize environment variables
 require('dotenv').config();
 
-const path = process.env.PATH || '/graphql';
+const path = process.env.GRAPHQL_PATH || '/graphql';
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -17,13 +17,14 @@ app.use(helmet());
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+	resolvers,
+	context : req => ({locale:'kr'})
 });
 
 server.applyMiddleware({app, path});
 
 app.listen(port, () => {
-	console.log(`GraphQL Server is now running on http://localhost:${port}/graphql`);
+	console.log(`GraphQL Server is now running on http://localhost:${port}/${path}`);
 }).on('error', err => {
 	if (err.message === `bind EADDRINUSE null:${port}`) {
 		console.error(`${port} 포트 바인딩 에러, 포트가 사용되고 있지 않은지 확인하세요`);
